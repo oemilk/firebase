e70c002914f225bb1883097cc7eb69cae88cca31
oemilk <oemilk@naver.com>
2016-07-31 15:04:52 +0900

    Implement Firebase Authentication feature that using google sign in

--- app/src/main/java/com/sh/firebase/AuthenticationFragment.java
+++ app/src/main/java/com/sh/firebase/AuthenticationFragment.java
@@ -1,5 +1,7 @@
 package com.sh.firebase;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -9,153 +11,246 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.concurrent.Executor;
 
 public class AuthenticationFragment extends Fragment {
 
-	private final String TAG = "AuthenticationFragment";
+    private final String TAG = "AuthenticationFragment";
 
-	private FirebaseAuth mFirebaseAuth;
-	private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private final String WEB_CLIENT_ID = "928488830433-camll1e451a5i4a0vrart4aia0rq707i.apps.googleusercontent.com";
+    private final int RC_SIGN_IN = 0;
 
-	private TextView mTextView;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
-	public AuthenticationFragment() {
-		// Required empty public constructor
-	}
+    private GoogleApiClient mGoogleApiClient;
 
-	public static AuthenticationFragment newInstance() {
-		AuthenticationFragment fragment = new AuthenticationFragment();
-		return fragment;
-	}
+    private TextView mPasswordTextView;
+    private TextView mGoogleSignInTextView;
+    private TextView mAuthStateTextview;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    public AuthenticationFragment() {
+        // Required empty public constructor
+    }
 
-		initFBAuthentication();
-	}
+    public static AuthenticationFragment newInstance() {
+        AuthenticationFragment fragment = new AuthenticationFragment();
+        return fragment;
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_authentication, container, false);
-		mTextView = (TextView) v.findViewById(R.id.authenticaion_textview);
-		Button signUpButton = (Button) v.findViewById(R.id.sign_up_button);
-		Button signInButton = (Button) v.findViewById(R.id.sign_in_button);
-		Button signOutButton = (Button) v.findViewById(R.id.sign_out_button);
-		signUpButton.setOnClickListener(mOnClickListener);
-		signInButton.setOnClickListener(mOnClickListener);
-		signOutButton.setOnClickListener(mOnClickListener);
-		return v;
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		mFirebaseAuth.addAuthStateListener(mAuthStateListener);
-	}
+        initFBPasswordAuthentication();
+        initFBGoogleSignIn();
+        initFBAuthState();
+    }
 
-	@Override
-	public void onStop() {
-		super.onStop();
-		if (mAuthStateListener != null) {
-			mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
-		}
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_authentication, container, false);
+        mPasswordTextView = (TextView) v.findViewById(R.id.pw_authenticaion_textview);
+        mGoogleSignInTextView = (TextView) v.findViewById(R.id.google_sign_in_textview);
+        mAuthStateTextview = (TextView) v.findViewById(R.id.auth_state_textview);
+        Button pwSignUpButton = (Button) v.findViewById(R.id.pw_sign_up_button);
+        Button pwSignInButton = (Button) v.findViewById(R.id.pw_sign_in_button);
+        Button pwSignOutButton = (Button) v.findViewById(R.id.pw_sign_out_button);
+        Button googlesignInButton = (Button) v.findViewById(R.id.google_sign_in_button);
+        Button googlesignOutButton = (Button) v.findViewById(R.id.google_sign_out_button);
 
-	private View.OnClickListener mOnClickListener = new View.OnClickListener() {
-		@Override
-		public void onClick(View view) {
-			int id = view.getId();
-			switch (id) {
-				case R.id.sign_up_button:
-					createUserWithEmailAndPassword();
-					break;
-				case R.id.sign_in_button:
-					signInWithEmailAndPassword();
-					break;
-				case R.id.sign_out_button:
-					signOut();
-					break;
-			}
-		}
-	};
+        pwSignUpButton.setOnClickListener(mOnClickListener);
+        pwSignInButton.setOnClickListener(mOnClickListener);
+        pwSignOutButton.setOnClickListener(mOnClickListener);
+        googlesignInButton.setOnClickListener(mOnClickListener);
+        googlesignOutButton.setOnClickListener(mOnClickListener);
+        return v;
+    }
 
-	private void initFBAuthentication() {
-		mFirebaseAuth = FirebaseAuth.getInstance();
-		mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-			@Override
-			public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-				FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-				String message;
-				if (firebaseUser != null) {
-					message= "onAuthStateChanged signed in : " + firebaseUser.getUid();
-				} else {
-					message= "onAuthStateChanged signed out";
-				}
-				mTextView.setText(message);
-			}
-		};
-	}
+    @Override
+    public void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
 
-	private void createUserWithEmailAndPassword() {
-		String email = "oemilk@naver.com"; // email address format
-		String password = "123456"; // at least 6 characters
-		mFirebaseAuth.createUserWithEmailAndPassword(email, password)
-				.addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
-					@Override
-					public void onComplete(@NonNull Task<AuthResult> task) {
-						String message;
-						if (task.isSuccessful()) {
-							message = "success createUserWithEmailAndPassword";
-						} else {
-							message = "fail createUserWithEmailAndPassword";
-						}
-						mTextView.setText(message);
-					}
-				}).addOnFailureListener(new OnFailureListener() {
-			@Override
-			public void onFailure(@NonNull Exception e) {
-				mTextView.setText(e.getMessage());
-				e.printStackTrace();
-			}
-		});
-	}
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
 
-	private void signInWithEmailAndPassword() {
-		String email = "test@test.com";
-		String password = "123456";
-		mFirebaseAuth.signInWithEmailAndPassword(email, password)
-				.addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
-					@Override
-					public void onComplete(@NonNull Task<AuthResult> task) {
-						String message;
-						if (task.isSuccessful()) {
-							message = "success signInWithEmailAndPassword";
-						} else {
-							message = "fail signInWithEmailAndPassword";
-						}
-						mTextView.setText(message);
-					}
-				}).addOnFailureListener(new OnFailureListener() {
-			@Override
-			public void onFailure(@NonNull Exception e) {
-				mTextView.setText(e.getMessage());
-				e.printStackTrace();
-			}
-		});
-	}
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int id = view.getId();
+            switch (id) {
+                case R.id.pw_sign_up_button:
+                    createUserWithEmailAndPassword();
+                    break;
+                case R.id.pw_sign_in_button:
+                    signInWithEmailAndPassword();
+                    break;
+                case R.id.pw_sign_out_button:
+                    signOut();
+                    break;
+                case R.id.google_sign_in_button:
+                    signInWithGoogleSignIn();
+                    break;
+                case R.id.google_sign_out_button:
+                    signOut();
+                    break;
+            }
+        }
+    };
 
-	private void signOut() {
-		if (mFirebaseAuth != null) {
-			mFirebaseAuth.signOut();
-		}
-	}
+    private void initFBPasswordAuthentication() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    private void initFBAuthState() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                String message;
+                if (firebaseUser != null) {
+                    message = "onAuthStateChanged signed in : " + firebaseUser.getUid();
+                } else {
+                    message = "onAuthStateChanged signed out";
+                }
+                mAuthStateTextview.setText(message);
+            }
+        };
+    }
+
+    private void createUserWithEmailAndPassword() {
+        String email = "oemilk@naver.com"; // email address format
+        String password = "123456"; // at least 6 characters
+        mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        String message;
+                        if (task.isSuccessful()) {
+                            message = "success createUserWithEmailAndPassword";
+                        } else {
+                            message = "fail createUserWithEmailAndPassword";
+                        }
+                        mPasswordTextView.setText(message);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                mPasswordTextView.setText(e.getMessage());
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void signInWithEmailAndPassword() {
+        String email = "test@test.com";
+        String password = "123456";
+        mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        String message;
+                        if (task.isSuccessful()) {
+                            message = "success signInWithEmailAndPassword";
+                        } else {
+                            message = "fail signInWithEmailAndPassword";
+                        }
+                        mPasswordTextView.setText(message);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                mPasswordTextView.setText(e.getMessage());
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void signOut() {
+        if (mFirebaseAuth != null) {
+            mFirebaseAuth.signOut();
+        }
+    }
+
+    private void initFBGoogleSignIn() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(WEB_CLIENT_ID)
+                .requestEmail()
+                .build();
+
+        Context context = getContext();
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .enableAutoManage(getActivity(), new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        mGoogleSignInTextView.setText(connectionResult.getErrorMessage());
+                    }
+                }).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+    }
+
+    private void signInWithGoogleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                mGoogleSignInTextView.setText("failed");
+            }
+        }
+    }
+
+	private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+		AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        String message;
+                        if (!task.isSuccessful()) {
+                            message = "success firebaseAuthWithGoogle";
+                        } else {
+                            message = "fail firebaseAuthWithGoogle";
+                        }
+                        mGoogleSignInTextView.setText(message);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                mGoogleSignInTextView.setText(e.getMessage());
+                e.printStackTrace();
+            }
+        });
+    }
 
 }
