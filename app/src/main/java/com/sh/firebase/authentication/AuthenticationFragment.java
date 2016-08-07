b90f8bd883d470bbbbdd2f256907c30bebde405c
oemilk <oemilk@naver.com>
2016-08-07 17:31:56 +0900

    Implement Firebase Authentication feature that using twitter log in

--- app/src/main/java/com/sh/firebase/authentication/AuthenticationFragment.java
+++ app/src/main/java/com/sh/firebase/authentication/AuthenticationFragment.java
@@ -15,6 +15,7 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
@@ -32,15 +33,33 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.TwitterAuthProvider;
 import com.sh.firebase.R;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import io.fabric.sdk.android.Fabric;
 
 public class AuthenticationFragment extends Fragment {
 
     private final String TAG = "AuthenticationFragment";
 
+    // for google sign in
     private final String WEB_CLIENT_ID = "928488830433-camll1e451a5i4a0vrart4aia0rq707i.apps.googleusercontent.com";
     private final int GOOGLE_SIGN_IN_REQUEST_CODE = 0;
+
+    // for facebook log in
     private final int FACEBOOK_LOG_IN_REQUEST_CODE = 64206;
+
+    // for twitter log in
+    private final String TWITTER_CONSUMER_KEY = "NDPH7edclsp7gK4GVPDPKwKGm";
+    private final String TWITTER_CONSUMER_SECRET = "Wjne0Ct1WLipHGdn1q2tX529xG3tEyss1RVFjl7dD9HY6e7z1m";
+    private final int TWITTER_LOG_IN_REQUEST_CODE = 140;
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
@@ -51,9 +70,11 @@ public class AuthenticationFragment extends Fragment {
     private TextView mPasswordTextView;
     private TextView mGoogleSignInTextView;
     private TextView mFaceBookLogInTextView;
+    private TextView mTwitterLogInTextView;
     private TextView mAuthStateTextview;
 
     private LoginButton mFacebookLoginButton;
+    private TwitterLoginButton mTwitterLoginButton;
 
     public AuthenticationFragment() {
         // Required empty public constructor
@@ -68,8 +89,9 @@ public class AuthenticationFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        initFBPasswordAuthentication();
+        initFBAuthentication();
         initFBGoogleSignIn();
+        initFBTwitterLogIn();
         initFBAuthState();
     }
 
@@ -81,21 +103,28 @@ public class AuthenticationFragment extends Fragment {
         mGoogleSignInTextView = (TextView) v.findViewById(R.id.google_sign_in_textview);
         mAuthStateTextview = (TextView) v.findViewById(R.id.auth_state_textview);
         mFaceBookLogInTextView = (TextView) v.findViewById(R.id.facebook_login_textview);
+        mTwitterLogInTextView = (TextView) v.findViewById(R.id.twitter_login_textview);
 
         Button pwSignUpButton = (Button) v.findViewById(R.id.pw_sign_up_button);
         Button pwSignInButton = (Button) v.findViewById(R.id.pw_sign_in_button);
         Button pwSignOutButton = (Button) v.findViewById(R.id.pw_sign_out_button);
         Button googlesignInButton = (Button) v.findViewById(R.id.google_sign_in_button);
         Button googlesignOutButton = (Button) v.findViewById(R.id.google_sign_out_button);
+        Button facebookLogOutButton = (Button) v.findViewById(R.id.facebook_logout_button);
+        Button twitterLogOutButton = (Button) v.findViewById(R.id.twitter_logout_button);
 
         mFacebookLoginButton = (LoginButton) v.findViewById(R.id.facebook_login_button);
+        mTwitterLoginButton = (TwitterLoginButton) v.findViewById(R.id.twitter_login_button);
         initFBFacebookLogIn();
+        setTwitterLogInCallback();
 
         pwSignUpButton.setOnClickListener(mOnClickListener);
         pwSignInButton.setOnClickListener(mOnClickListener);
         pwSignOutButton.setOnClickListener(mOnClickListener);
         googlesignInButton.setOnClickListener(mOnClickListener);
         googlesignOutButton.setOnClickListener(mOnClickListener);
+        facebookLogOutButton.setOnClickListener(mOnClickListener);
+        twitterLogOutButton.setOnClickListener(mOnClickListener);
         return v;
     }
 
@@ -110,6 +139,24 @@ public class AuthenticationFragment extends Fragment {
         super.onStop();
         if (mAuthStateListener != null) {
             mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GOOGLE_SIGN_IN_REQUEST_CODE) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                mGoogleSignInTextView.setText("failed");
+            }
+        } else if (requestCode == FACEBOOK_LOG_IN_REQUEST_CODE) {
+            mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
+        } else if (requestCode == TWITTER_LOG_IN_REQUEST_CODE) {
+            mTwitterLoginButton.onActivityResult(requestCode, resultCode, data);
         }
     }
 
@@ -133,11 +180,17 @@ public class AuthenticationFragment extends Fragment {
                 case R.id.google_sign_out_button:
                     signOut();
                     break;
+                case R.id.facebook_logout_button:
+                    logOutWithFacebook();
+                    break;
+                case R.id.twitter_logout_button:
+                    logOutWithTwitter();
+                    break;
             }
         }
     };
 
-    private void initFBPasswordAuthentication() {
+    private void initFBAuthentication() {
         mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
@@ -157,6 +210,7 @@ public class AuthenticationFragment extends Fragment {
         };
     }
 
+    // [START auth_with_email_and_password]
     private void createUserWithEmailAndPassword() {
         String email = "oemilk@naver.com"; // email address format
         String password = "123456"; // at least 6 characters
@@ -210,7 +264,9 @@ public class AuthenticationFragment extends Fragment {
             mFirebaseAuth.signOut();
         }
     }
+    // [END auth_with_email_and_password]
 
+    // [START auth_with_google]
     private void initFBGoogleSignIn() {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(WEB_CLIENT_ID)
@@ -230,22 +286,6 @@ public class AuthenticationFragment extends Fragment {
     private void signInWithGoogleSignIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, GOOGLE_SIGN_IN_REQUEST_CODE);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == GOOGLE_SIGN_IN_REQUEST_CODE) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                GoogleSignInAccount account = result.getSignInAccount();
-                firebaseAuthWithGoogle(account);
-            } else {
-                mGoogleSignInTextView.setText("failed");
-            }
-        } else if (requestCode == FACEBOOK_LOG_IN_REQUEST_CODE) {
-            mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
-        }
     }
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
@@ -270,7 +310,9 @@ public class AuthenticationFragment extends Fragment {
             }
         });
     }
+    // [END auth_with_google]
 
+    // [START auth_with_facebook]
     private void initFBFacebookLogIn() {
         mFacebookCallbackManager = CallbackManager.Factory.create();
         mFacebookLoginButton.setFragment(this);
@@ -311,5 +353,70 @@ public class AuthenticationFragment extends Fragment {
                     }
                 });
     }
+
+    private void logOutWithFacebook() {
+        if (mFirebaseAuth != null) {
+            LoginManager.getInstance().logOut();
+            signOut();
+            mFaceBookLogInTextView.setText("facebook log out");
+        }
+    }
+    // [END auth_with_facebook]
+
+    // [START auth_with_twitter]
+    private void initFBTwitterLogIn() {
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
+        Fabric.with(AuthenticationFragment.this.getContext(), new Twitter(authConfig));
+    }
+
+    private void setTwitterLogInCallback() {
+        mTwitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                handleTwitterSession(result.data);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                mTwitterLogInTextView.setText(exception.getMessage());
+                exception.printStackTrace();
+            }
+        });
+    }
+
+    private void handleTwitterSession(TwitterSession session) {
+        AuthCredential credential = TwitterAuthProvider.getCredential(
+                session.getAuthToken().token,
+                session.getAuthToken().secret);
+
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(AuthenticationFragment.this.getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        String message;
+                        if (task.isSuccessful()) {
+                            message = "success firebaseAuthWithGoogle";
+                        } else {
+                            message = "fail firebaseAuthWithGoogle";
+                        }
+                        mTwitterLogInTextView.setText(message);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                mTwitterLogInTextView.setText(e.getMessage());
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void logOutWithTwitter() {
+        if (mFirebaseAuth != null) {
+            Twitter.logOut();
+            signOut();
+            mTwitterLogInTextView.setText("twitter log out");
+        }
+    }
+    // [END auth_with_twitter]
 
 }
--- app/src/main/java/com/sh/firebase/main/MainActivity.java
+++ app/src/main/java/com/sh/firebase/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sh.firebase.main;
 
+import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
@@ -109,5 +110,14 @@ public class MainActivity extends AppCompatActivity {
 				}
 			};
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_layout);
+		if (fragment != null) {
+			fragment.onActivityResult(requestCode, resultCode, data);
+		}
+	}
 
 }
