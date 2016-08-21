56be83d4b088570f106949e5b375eec32d6d60e9
pooq-shlee <shlee@captv.co.kr>
2016-08-21 21:51:08 +0900

    Implement Firebase Authentication feature for linking multiple auth providers

--- app/src/main/java/com/sh/firebase/authentication/AuthenticationForAnonymousFragment.java
+++ app/src/main/java/com/sh/firebase/authentication/AuthenticationForAnonymousFragment.java
@@ -10,14 +10,13 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.EmailAuthProvider;
-import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.sh.firebase.R;
 
 public class AuthenticationForAnonymousFragment extends Fragment {
@@ -39,6 +38,15 @@ public class AuthenticationForAnonymousFragment extends Fragment {
                     break;
                 case R.id.anonymous_convert_button:
                     convertToPermanentAccount();
+                    break;
+                case R.id.signin_button:
+                    signInWithEmailAndPassword();
+                    break;
+                case R.id.link_button:
+                    link();
+                    break;
+                case R.id.unlink_button:
+                    unlink();
                     break;
             }
         }
@@ -68,8 +76,14 @@ public class AuthenticationForAnonymousFragment extends Fragment {
         mAuthStateTextview = (TextView) v.findViewById(R.id.auth_state_textview);
         Button anonymousSignInButton = (Button) v.findViewById(R.id.anonymous_sign_in_button);
         Button anonymousConvertButton = (Button) v.findViewById(R.id.anonymous_convert_button);
+        Button signInButton = (Button) v.findViewById(R.id.signin_button);
+        Button linkButton = (Button) v.findViewById(R.id.link_button);
+        Button unlinkButton = (Button) v.findViewById(R.id.unlink_button);
         anonymousSignInButton.setOnClickListener(mOnClickListener);
         anonymousConvertButton.setOnClickListener(mOnClickListener);
+        signInButton.setOnClickListener(mOnClickListener);
+        linkButton.setOnClickListener(mOnClickListener);
+        unlinkButton.setOnClickListener(mOnClickListener);
         return v;
     }
 
@@ -143,6 +157,53 @@ public class AuthenticationForAnonymousFragment extends Fragment {
                         mAuthStateTextview.setText(message);
                     }
                 });
+    }
+
+    private void signInWithEmailAndPassword() {
+        String email = "test@test.com";
+        String password = "123456";
+        mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        String message;
+                        if (task.isSuccessful()) {
+                            message = "success signInWithEmailAndPassword";
+                        } else {
+                            message = "fail signInWithEmailAndPassword";
+                        }
+                        mAuthStateTextview.setText(message);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                mAuthStateTextview.setText(e.getMessage());
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void link() {
+        convertToPermanentAccount();
+    }
+
+    private void unlink() {
+        if (mFirebaseAuth.getCurrentUser() != null) {
+            String providerId = mFirebaseAuth.getCurrentUser().getProviderId();
+            FirebaseAuth.getInstance().getCurrentUser().unlink(providerId)
+                    .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            String message;
+                            if (!task.isSuccessful()) {
+                                message = "unlink faile : " + task.getException();
+                            } else {
+                                message = "unlink success";
+                            }
+                            mAuthStateTextview.setText(message);
+                        }
+                    });
+        }
     }
 
 }
